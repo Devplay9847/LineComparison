e0f0a1737dd826367214246c050c6f07dd88d417
Devplay9847 <devendra.nishad.9847@gmail.com>
2022-05-01 12:11:11 +0530

    when two lines are the equal.

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -1,29 +1,96 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
-namespace LineComparison
+// C# Implementation. To find the point of 
+// intersection of two lines 
+
+
+// Class used to  used to store the X and Y 
+// coordinates of a point respectively 
+public class Point
 {
-	internal class Program
+	public double x, y;
+
+
+	public Point(double x, double y)
 	{
-		static void Main(string[] args)
+		this.x = x;
+		this.y = y;
+	}
+
+
+	// Method used to display X and Y coordinates 
+	// of a point 
+	public static void displayPoint(Point p)
+	{
+		Console.WriteLine("(" + p.x + ", " + p.y + ")");
+		Console.ReadLine();
+	}
+}
+
+
+public class Test
+{
+	public static Point lineLineIntersection(Point A, Point B, Point C, Point D)
+	{
+		// Line AB represented as a1x + b1y = c1 
+		double a1 = B.y - A.y;
+		double b1 = A.x - B.x;
+		double c1 = a1 * (A.x) + b1 * (A.y);
+
+
+		// Line CD represented as a2x + b2y = c2 
+		double a2 = D.y - C.y;
+		double b2 = C.x - D.x;
+		double c2 = a2 * (C.x) + b2 * (C.y);
+
+
+		double determinant = a1 * b2 - a2 * b1;
+
+
+		if (determinant == 0)
 		{
-			double x1, y1, x2, y2;
-			Console.WriteLine("Input line one x1: ");
-			x1 = Convert.ToDouble(Console.ReadLine());
-			Console.WriteLine("Input line one y1: ");
-			y1 = Convert.ToDouble(Console.ReadLine());
-			Console.WriteLine("Input line one x2: ");
-			x2 = Convert.ToDouble(Console.ReadLine());
-			Console.WriteLine("Input line one x2: ");
-			y2 = Convert.ToDouble(Console.ReadLine());
-			double LengthofLine = 0;
-			LengthofLine = Math.Sqrt((x2 - x1) * 2 + (y2 - y1) * 2);
-			Console.WriteLine("Lenght of line =" + LengthofLine);
+			// The lines are parallel. This is simplified 
+			// by returning a pair of FLT_MAX 
+			return new Point(double.MaxValue, double.MaxValue);
+		}
+		else
+		{
+			double x = (b2 * c1 - b1 * c2) / determinant;
+			double y = (a1 * c2 - a2 * c1) / determinant;
+			return new Point(x, y);
+		}
+	}
+
+
+	// Driver method 
+	public static void Main(string[] args)
+	{
+		Point A = new Point(1, 1);
+		Point B = new Point(4, 4);
+		Point C = new Point(1, 8);
+		Point D = new Point(2, 4);
+
+
+		Point intersection = lineLineIntersection(A, B, C, D);
+
+
+		if (intersection.x == double.MaxValue && intersection.y == double.MaxValue)
+		{
+			Console.WriteLine("The given lines AB and CD are parallel.");
+		}
+
+
+		else
+		{
+			// NOTE: Further check can be applied in case 
+			// of line segments. Here, we have considered AB 
+			// and CD as lines 
+			Console.Write("The intersection of the given lines AB " + "and CD is: ");
+			Point.displayPoint(intersection);
 			Console.ReadLine();
+
+
 		}
 	}
 }
