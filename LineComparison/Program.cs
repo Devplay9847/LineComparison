eaade3e791d8d691f080b8ecf4b7303b1f6ebebb
Devplay9847 <devendra.nishad.9847@gmail.com>
2022-05-01 12:06:31 +0530

    calculate the lenthe of the two points

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace LineComparison
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Start with Displaying Welcome to Line Comparison ComputationProgram on Master Branch");
-            Console.ReadLine();
-        }
-    }
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double x1, y1, x2, y2;
+			Console.WriteLine("Input line one x1: ");
+			x1 = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Input line one y1: ");
+			y1 = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Input line one x2: ");
+			x2 = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Input line one x2: ");
+			y2 = Convert.ToDouble(Console.ReadLine());
+			double LengthofLine = 0;
+			LengthofLine = Math.Sqrt((x2 - x1) * 2 + (y2 - y1) * 2);
+			Console.WriteLine("Lenght of line =" + LengthofLine);
+			Console.ReadLine();
+		}
+	}
 }
