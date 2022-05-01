5ad0d0232b32e9bd2681f2138c3337b0c8a88daf
Devplay9847 <devendra.nishad.9847@gmail.com>
2022-05-01 12:02:14 +0530

    Welcome to Line Comparison Computation Program on Master Branch

---
+++ LineComparison/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start with Displaying Welcome to Line Comparison ComputationProgram on Master Branch");
+            Console.ReadLine();
+        }
+    }
+}
